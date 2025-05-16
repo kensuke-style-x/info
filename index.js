4a4f28b8ad7062328c621e634bf792af208b52ea
kensuke-style-x <66291400+kensuke-style-x@users.noreply.github.com>
2025-05-16 11:07:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-function showLanguage(language){
-  const languages = [`en', `jp'];
+function showLanguage(lang){
+  const languages = [`en`, `jp`];
   languages.forEach(id =>{
     const section = document.getElementById(id);
     if (section) {
-      section.classList.remove(`active');
+      section.classList.remove(`active`);
   }
 });
 
-  const targetSection = document.getElementById(language);
+  const targetSection = document.getElementById(lang);
   if (targetSection) {
-     targetSection.classList.add(`active');
+     targetSection.classList.add(`active`);
     }
  
   }
+
+document.addEventListner(`DOMContentLoaded`, () => {
+  document.getElementById(`en-btn`).addEventListener(`click`, () => showLanguage(`en`));
+  document.getElementById(`jp-btn`).addEventListener(`click`, () => showLanguage(`jp`));
+});
 
  
     
